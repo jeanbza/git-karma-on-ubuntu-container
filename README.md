3190fe31fce933cfd3a8978ece43f94bb999d7f4
Travis Patterson <tpatterson@pivotal.io>
2016-11-22 09:38:36 -0700

    Simple test works

---
+++ README.md
@@ -0,0 +1,5 @@
+# karma-in-docker
+
+1. `npm install`
+1. `npm test`
+1. `docker build . -t karma-docker`
---
+++ karma.conf.js
@@ -0,0 +1,17 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: ['tests/*.js'],
+    exclude: [],
+    preprocessors: {},
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    concurrency: Infinity
+  })
+}
